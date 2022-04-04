af49b89b86bd2bc01fd1bbd3a9741cc07dae411b
Geoslim <iedomwande@yahoo.com>
2022-04-04 12:13:04 +0100

    update permissions for admin

--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,8 @@ Route::get('history', 'CPInvestController@getInvestmentHistory');
 Route::get('{investmentId}/details', 'CPInvestController@getInvestmentDetails');
 Route::get('active-investment-summary', 'CPInvestController@activeInvestmentSummary');
 Route::get(
-    '{investmentId}/transactions',
-    'CPInvestController@getInvestmentTransactions'
+	'{investmentId}/transactions',
+	'CPInvestController@getInvestmentTransactions'
 );
 
 Route::post('otp', 'CPInvestController@requestOtp');
@@ -29,9 +29,9 @@ Route::post('{investmentId}/withdraw', 'CPInvestController@withdrawFunds');
 
 /** ========== Admin Expense Route ========= **/
 Route::group([
-    'namespace' => 'Admin',
-    'prefix' => 'admin',
-    'middleware' => ['auth:api']
+	'namespace' => 'Admin',
+	'prefix' => 'admin',
+	'middleware' => ['auth:api']
 ], function () {
 	Route::get('rates', 'CPInvestController@getAdminRates');
 	Route::get('rates/update', 'CPInvestController@updateAdminRates');
@@ -39,11 +39,14 @@ Route::group([
 	Route::get('tenure', 'CPInvestController@getAdminTenure');
 	Route::get('tenure/update', 'CPInvestController@updateAdminTenure');
 
-	Route::get('configuration', 'CPInvestController@getAdminConfiguration');
-	Route::post('configuration/update', 'CPInvestController@updateAdminConfiguration');
+	Route::group(['prefix' => 'configuration', 'middleware' => ['permission:credpal:can_view_invest_configurations']], function () {
+		Route::get('', 'CPInvestController@getAdminConfiguration');
+		Route::post('update', 'CPInvestController@updateAdminConfiguration');
+	});
 
-    Route::get('investments', 'CPInvestController@getAllInvestments');
+	Route::group(['prefix' => 'investments', 'middleware' => ['permission:credpal:can_view_invest']], function () {
+		Route::get('', 'CPInvestController@getAllInvestments');
+	});
 });
-
 
 Route::post('webhook', 'CPInvestController@webhook')->withoutMiddleware('auth:api');
