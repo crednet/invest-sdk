712cef01ba09f6e1fc4002e6950a3e9a917b58ad
Geoslim <iedomwande@yahoo.com>
2022-04-05 20:55:22 +0100

    minor refactor

--- src/CPInvest.php
+++ src/CPInvest.php
@@ -7,75 +7,75 @@ use Credpal\CPInvest\Services\InvestService;
 
 class CPInvest
 {
-    public function create(array $data)
-    {
-        return InvestService::createInvestment($data);
-    }
+	public function create(array $data)
+	{
+		return InvestService::createInvestment($data);
+	}
 
-    public function getAllUserInvestments($data)
-    {
-        return InvestService::getAllUserInvestments($data);
-    }
+	public function getAllUserInvestments($data)
+	{
+		return InvestService::getAllUserInvestments($data);
+	}
 
-    public function getUserActiveInvestments($data)
-    {
-        return InvestService::getUserActiveInvestments($data);
-    }
+	public function getUserActiveInvestments($data)
+	{
+		return InvestService::getUserActiveInvestments($data);
+	}
 
-    public function getInvestmentDetails($data)
-    {
-        return InvestService::getInvestmentDetails($data);
-    }
+	public function getInvestmentDetails($data)
+	{
+		return InvestService::getInvestmentDetails($data);
+	}
 
-    public function getInvestmentTransactions($data)
-    {
-        return InvestService::getInvestmentTransactions($data);
-    }
+	public function getInvestmentTransactions($data)
+	{
+		return InvestService::getInvestmentTransactions($data);
+	}
 
-    public function liquidateInvestment($data)
-    {
-        return InvestService::liquidateInvestment($data);
-    }
+	public function liquidateInvestment($data)
+	{
+		return InvestService::liquidateInvestment($data);
+	}
 
-    public function withdrawFunds($data)
-    {
-        return InvestService::withdrawFunds($data);
-    }
+	public function withdrawFunds($data)
+	{
+		return InvestService::withdrawFunds($data);
+	}
 
-    public function getTenures()
-    {
-        return InvestService::getInvestmentTenures();
-    }
+	public function getTenures()
+	{
+		return InvestService::getInvestmentTenures();
+	}
 
-    public function requestOtp($data)
-    {
-        return InvestService::requestOtp($data);
-    }
+	public function requestOtp($data)
+	{
+		return InvestService::requestOtp($data);
+	}
 
-    public function getInvestmentTypes()
-    {
-        return InvestService::getInvestmentTypes();
-    }
+	public function getInvestmentTypes()
+	{
+		return InvestService::getInvestmentTypes();
+	}
 
-    public function getInvestmentTypeDetails($investmentTypeSlug)
-    {
-        return InvestService::getInvestmentTypeDetails($investmentTypeSlug);
-    }
+	public function getInvestmentTypeDetails($investmentTypeSlug)
+	{
+		return InvestService::getInvestmentTypeDetails($investmentTypeSlug);
+	}
 
-    public function getSummary($data)
-    {
-        return InvestService::getSummary($data);
-    }
+	public function getSummary($data)
+	{
+		return InvestService::getSummary($data);
+	}
 
-    public function getRate($data)
-    {
-        return InvestService::getRate($data);
-    }
+	public function getRate($data)
+	{
+		return InvestService::getRate($data);
+	}
 
-    public function getTransformRates()
-    {
-        return InvestService::getTransformRates();
-    }
+	public function getTransformRates()
+	{
+		return InvestService::getTransformRates();
+	}
 
 	public static function getAdminRates()
 	{
@@ -107,8 +107,13 @@ class CPInvest
 		return InvestService::updateAdminConfiguration($key, $data);
 	}
 
-    public function getInvestmentHistory($data)
-    {
-        return InvestService::getInvestmentHistory($data);
-    }
+	public function getInvestmentHistory($data)
+	{
+		return InvestService::getInvestmentHistory($data);
+	}
+
+	public static function getAllInvestments()
+	{
+		return InvestService::getAllInvestments();
+	}
 }
--- src/Facade/CpInvest.php
+++ src/Facade/CpInvest.php
@@ -4,13 +4,20 @@ namespace Credpal\CPInvest\Facade;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static mixed getAdminConfiguration()
+ * @method static mixed updateAdminConfiguration(string $key, array $data)
+ * @method static mixed getAllInvestments()
+ *
+ * @see \CredPal\CPInvest\CPInvest
+ */
 class CpInvest extends Facade
 {
-    /**
-     * @return string
-     */
-    protected static function getFacadeAccessor(): string
-    {
-        return 'cpinvest';
-    }
+	/**
+	 * @return string
+	 */
+	protected static function getFacadeAccessor(): string
+	{
+		return 'cpinvest';
+	}
 }
